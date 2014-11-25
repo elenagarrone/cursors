eac4d3a5d05778b1507389e0513140c1b72f65f9
elena <elena.15@hotmail.it>
2014-11-25 12:08:54 +0000

    event listener is turning on and off

--- cursors.js
+++ cursors.js
@@ -15,14 +15,29 @@ if (Meteor.isClient) {
     Hammer(el).on('press', function(event){
       console.log('Create Mongo Object')
       Pointer.insert({_id: id._str, x: 0, y: 0})
+      startMovementCapture()
     })
 
     Hammer(el).on('hammer.input', function(event){
       if (event.isFirst === false){
         Pointer.remove(id._str)
         console.log('Delete Mongo Object')
+        stopMovementCapture()
       }
     })
+  }
+
+  function writeCoordinance(motion){
+    console.log(motion.gamma)
+  }
+
+  function startMovementCapture(){
+    window.addEventListener('deviceorientation', writeCoordinance, false)
+  }
+
+  function stopMovementCapture(){
+    window.removeEventListener('deviceorientation', writeCoordinance, false)
+
   }
 
 }
